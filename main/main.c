e53270245028a9f617a87129f31c273a726593c5
Andrew N. Shalaev <isqad88@gmail.com>
2022-12-16 19:45:36 +0500

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,27 @@
+/* WiFi camera Telegram bot
+
+   This code is in the MIT licensed
+*/
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "wifi_sta.h"
+
+void app_main(void)
+{
+  // Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+  {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  // Connect to Wifi
+  ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+  wifi_init_sta();
+}
