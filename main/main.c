d0584644bbfa57a758b861af43d6600e76a921dd
Andy0x58 <isqad88@gmail.com>
2022-12-17 14:57:30 +0500

    fix: revoke tg bot token

--- main/main.c
+++ main/main.c
@@ -16,7 +16,10 @@
 #define HTTP_CLIENT_TIMEOUT_MS 5000
 #define HTTP_CLIENT_MAX_REDIRECTS 3
 #define HTTP_CLIENT_USER_AGENT "ESP32-Wifi-Camera Telegram Bot v1.0.0 (Home automation by Andy0x58)"
-#define TG_API_URL "https://api.telegram.org/bot689500456:AAGl4r4t2j-LwcsVIYduzJJCR1MTZBCgYUY/getUpdates"
+
+#define TG_API_BOT_ENDPOINT             "https://api.telegram.org/bot"
+#define TG_API_BOT_GET_UPDATES_COMMAND  "/getUpdates"
+#define TG_GET_UDATES_URL               TG_API_BOT_ENDPOINT CONFIG_ESP_TG_BOT_TOKEN TG_API_BOT_GET_UPDATES_COMMAND
 
 static char* http_response_buffer;
  
@@ -40,7 +43,7 @@ static esp_err_t http_client_event_handler(esp_http_client_event_t* event)
     return ESP_FAIL;
     break;
   case HTTP_EVENT_ON_CONNECTED:
-    ESP_LOGI(TAG, "Successfully connected to %s", TG_API_URL);
+    ESP_LOGI(TAG, "Successfully connected to %s", TG_GET_UDATES_URL);
     break;
   case HTTP_EVENT_HEADERS_SENT:
     break;
@@ -87,10 +90,10 @@ static esp_err_t http_client_event_handler(esp_http_client_event_t* event)
       content_length = 0;
     }
 
-    ESP_LOGI(TAG, "Finish http session with %s", TG_API_URL);
+    ESP_LOGI(TAG, "Finish http session with %s", TG_GET_UDATES_URL);
     break;
   case HTTP_EVENT_DISCONNECTED:
-    ESP_LOGW(TAG, "Disconnected from %s", TG_API_URL);
+    ESP_LOGW(TAG, "Disconnected from %s", TG_GET_UDATES_URL);
     if (http_response_buffer != NULL)
     {
       free(http_response_buffer);
@@ -105,6 +108,28 @@ static esp_err_t http_client_event_handler(esp_http_client_event_t* event)
   
 
   return ESP_OK;
+}
+
+void getUpdates(void)
+{
+  const esp_http_client_config_t client_conf = {
+    .url = TG_GET_UDATES_URL,
+    .keep_alive_enable = true,
+    .timeout_ms = HTTP_CLIENT_TIMEOUT_MS,
+    .user_agent = HTTP_CLIENT_USER_AGENT,
+    .max_redirection_count = HTTP_CLIENT_MAX_REDIRECTS,
+    .event_handler = &http_client_event_handler
+  };
+
+  esp_http_client_handle_t http_client_handle = esp_http_client_init(&client_conf);
+  if (!http_client_handle)
+  {
+    ESP_LOGE(TAG, "http_client_handle is NULL!");
+    ESP_LOGI(TAG, "Restart!");
+    return esp_restart();
+  }
+
+   ESP_ERROR_CHECK(esp_http_client_perform(http_client_handle));
 }
 
 void app_main(void)
@@ -124,22 +149,5 @@ void app_main(void)
   ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
   wifi_init_sta();
 
-  const esp_http_client_config_t client_conf = {
-    .url = TG_API_URL,
-    .keep_alive_enable = true,
-    .timeout_ms = HTTP_CLIENT_TIMEOUT_MS,
-    .user_agent = HTTP_CLIENT_USER_AGENT,
-    .max_redirection_count = HTTP_CLIENT_MAX_REDIRECTS,
-    .event_handler = &http_client_event_handler
-  };
-
-  esp_http_client_handle_t http_client_handle = esp_http_client_init(&client_conf);
-  if (!http_client_handle)
-  {
-    ESP_LOGE(TAG, "http_client_handle is NULL!");
-    ESP_LOGI(TAG, "Restart!");
-    return esp_restart();
-  }
-
-   ESP_ERROR_CHECK(esp_http_client_perform(http_client_handle));
+  getUpdates();
 }
